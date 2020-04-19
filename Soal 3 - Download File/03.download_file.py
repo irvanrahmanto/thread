646e65867c5b65e1296528ad5728c8379f74a4f9
Irvan Naufali Rahmanto <42972909+irvanrahmanto@users.noreply.github.com>
2020-04-19 16:43:21 +0700

    menambahkan komen pada line

--- Soal 3 - Download File/03.download_file.py
+++ Soal 3 - Download File/03.download_file.py
@@ -6,6 +6,7 @@ import urllib.error
 import urllib.parse
 import time
 
+# Mengimport library threading, os, requests dan lain lain nya
 url = "https://apod.nasa.gov/apod/image/1901/LOmbradellaTerraFinazzi.jpg"
 
 
@@ -15,6 +16,7 @@ def buildRange(value, numsplits):
         if i == 0:
             lst.append('%s-%s' % (i, int(round(1 + i * value /
                                                (numsplits*1.0) + value/(numsplits*1.0)-1, 0))))
+            # Menambahkan elemen pada array lst yang sudah dibuat
         else:
             lst.append('%s-%s' % (int(round(1 + i * value/(numsplits*1.0), 0)),
                                   int(round(1 + i * value/(numsplits*1.0) + value/(numsplits*1.0)-1, 0))))
@@ -33,6 +35,8 @@ class SplitBufferThreads(threading.Thread):
         self.__byteRange = byteRange
         self.req = None
 
+        # Memisahkan buffer pada tiap thread dengan byte pada range tertentu
+
     def run(self):
         self.req = urllib.request.Request(
             self.__url,  headers={'Range': 'bytes=%s' % self.__byteRange})
@@ -42,37 +46,67 @@ class SplitBufferThreads(threading.Thread):
 
 
 def main(url=None, splitBy=3):
+    # Memulai perintah recording waktu
     start_time = time.time()
+    # terminate ural jika url belum ada
     if not url:
         print("Please Enter some url to begin download.")
         return
 
+    # membagi nama file berdasarkan namanya dari path url posisi paling terakir
     fileName = url.split('/')[-1]
+
+    # Mengukur besarnya file yang didownload
     sizeInBytes = requests.head(
         url, headers={'Accept-Encoding': 'identity'}).headers.get('content-length', None)
+
+    # Mengoutputkan besarnya file
     print("%s bytes to download." % sizeInBytes)
+
+    # Jika isi pada byte kosong, maka program akan selesai
     if not sizeInBytes:
         print("Size cannot be determined.")
         return
 
+    # Membuat list baru
     dataLst = []
+
+    # Melakukan perulangan sejumlah splitBy
     for idx in range(splitBy):
+
+        # Membagi byte data sesuai dengan splitBy
         byteRange = buildRange(int(sizeInBytes), splitBy)[idx]
+
+        # Mengassign thread dengan job sesuai dari pembagian yang dilakukan pada byterange
         bufTh = SplitBufferThreads(url, byteRange)
+
+        # Memulai pengupload an
         bufTh.start()
+
+        # main thread menunggu pekerjaan childtread selesai untuk dapat mengeksekusi pekerjaan lain nya
         bufTh.join()
+
+        # Memasukkan data yang sudah dimasukan ke dalam list
         dataLst.append(bufTh.getFileData())
 
+    # Menggabungkan isi datalst
     content = b''.join(dataLst)
 
+    # Jika list dataLst berhasil terisi maka
     if dataLst:
+        # Jika dalam folder memiliki nama file yang sama
         if os.path.exists(fileName):
+            # Hapus file nya
             os.remove(fileName)
+        # Outputkan waktu pekerjaan
         print("--- %s seconds ---" % str(time.time() - start_time))
         with open(fileName, 'wb') as fh:
+            # Penulisan konten file
             fh.write(content)
+        # Output selesai
         print("Finished Writing file %s" % fileName)
 
 
 if __name__ == '__main__':
+    # Menjalankan program utama
     main(url)
